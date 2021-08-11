0549ee7014cb41a4cea4180683c6e98e807a6516
LizHuang <search.psop@gmail.com>
2021-08-11 12:30:26 +0800

    add initial setting

---
+++ script.js
@@ -0,0 +1,10 @@
+import {createApp, ref} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.1/vue.esm-browser.min.js'
+
+const app = createApp({
+    setup() {
+        
+        return {
+
+        }
+    }
+}).mount('#app')
