9f1efbc7c25f99bb5f550900d61fbfb9e01f57d2
LizHuang <search.psop@gmail.com>
2021-09-15 09:43:00 +0800

    add todo adding, deleting function

--- script.js
+++ script.js
@@ -2,9 +2,30 @@ import {createApp, ref} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.1/v
 
 const app = createApp({
     setup() {
+        const todo = ref('')
+        const todoList = ref([])
+        const status = ref('all')
+
+        function add() {
+            todoList.value.push({
+                id: todoList.value.length + 1,
+                text: todo.value,
+                completed: false, 
+            })
+            todo.value = ''
+        }
+
+        function remove(todo) {
+            const index = todoList.value.findIndex(obj => obj.id === todo.id)
+            todoList.value.splice(index, 1)
+        }
         
         return {
-
+            todo,
+            todoList,
+            status,
+            add,
+            remove,
         }
     }
 }).mount('#app')
